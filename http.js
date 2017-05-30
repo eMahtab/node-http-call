2e06920e04a17086ef0399fb48313c37431de892
Mahtab Alam <alammahtab08@gmail.com>
2017-05-30 13:41:34 +0530

    Initial Commit

---
+++ http.js
@@ -0,0 +1,33 @@
+var http=require('http');
+
+var options = {
+  protocol:'http:',  
+  host: 'localhost',
+  port:3000,
+  path: '/iso/country/Japan',
+  method:'GET'
+};
+
+var callback = function(response) {
+  var str = '';
+
+  //another chunk of data has been recieved, so append it to `str`
+  response.on('data', function (chunk) {
+    str += chunk;
+  });
+
+  //the whole response has been recieved, so we just print it out here
+  response.on('end', function () {
+    console.log(str);
+  });
+}
+
+var request=http.request(options, callback);
+
+request.on('error', function(err) {
+        // handle errors with the request itself
+        console.error('Error with the request:', err.message);        
+});
+
+request.end();
+
