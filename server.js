51f05ee2dfa387ace797a004de21a5975041581d
Mahtab Alam <alammahtab08@gmail.com>
2017-05-31 09:14:41 +0530

    Removed unnecessary API from server

--- server.js
+++ server.js
@@ -1,46 +1,8 @@
 var express=require('express');
-var app=express();
 var bodyParser=require('body-parser');
-
+var app=express();
 
 app.use(bodyParser.json({limit:'5mb'}));
-
-
-var olympic_ranking=[
-                     {"Ranking":3,"Country":"China"},
-                     {"Ranking":4,"Country":"Russia"},
-                     {"Ranking":5,"Country":"Germany"},
-                     {"Ranking":6,"Country":"Japan"},
-                     {"Ranking":7,"Country":"France"}
-                    ]
-
-function isEmpty(obj) {
-    for(var key in obj) {
-        if(obj.hasOwnProperty(key))
-            return false;
-    }
-    return true;
-}
-
-function get_rank(rank){
-    var data={};
-    for(var i=0,len=olympic_ranking.length;i<len;i++){
-      if(olympic_ranking[i]['Ranking'] == rank){
-        data=olympic_ranking[i];
-        break;
-      }
-    }
-    return data;
-}
-
-app.get('/olympic/2016/ranking/:rank',function(req,res){
-     var result=get_rank(req.params.rank);
-     if( !isEmpty(result) ){
-         res.status(200).send(result);
-     }else{
-         res.status(404).send('Record does not exist');
-     }
-});  
 
 
 var iso_lookup={"Germany":"DE","China":"CN","France":"FR","Japan":"JP","Russia":"RU"};
@@ -52,17 +14,6 @@ app.get('/iso/country/:country_name',function(req,res){
         res.status(404).send('Record does not exist');
       }
 });
-
-var medal_tally={"CN":70,"RU":56,"FR":42,"JP":41,"DE":42};
-
-app.get('/olympic/2016/medal/:country_iso',function(req,res){
-      if(medal_tally.hasOwnProperty(req.params.country_iso)){
-        res.status(200).send({"medal":medal_tally[req.params.country_iso]});
-      }else{
-        res.status(404).send('Record does not exist'); 
-      }
-});
-
 
 app.post('/samplePost',function(req,res){
   console.log("Post Body "+JSON.stringify(req.body));
